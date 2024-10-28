8b9a8181a16ab9c64ea74b53007cf0d370663443
nik <108171566+xdgreat@users.noreply.github.com>
2024-10-29 09:21:01 +1200

    added types

--- app/api/fetchTotalTrickOrTreaters/route.ts
+++ app/api/fetchTotalTrickOrTreaters/route.ts
@@ -3,10 +3,14 @@ import {  NextResponse } from "next/server";
 import db, { initializeDatabase } from "@/app/libs/db";
 
 initializeDatabase(); 
+type resultType ={
+  trick_count: number,
+  treat_count: number
+}
 
 export async function GET() {
   try {
-    const result:any = db.prepare("SELECT trick_count, treat_count FROM counts WHERE id = 1").get();
+    const result:resultType = db.prepare("SELECT trick_count, treat_count FROM counts WHERE id = 1").get();
     
     if (result) {
       const totalTrickOrTreaters = result.trick_count + result.treat_count;
